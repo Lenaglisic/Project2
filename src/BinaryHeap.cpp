b989e120637bc7f639bd1e1bda94c71e948d3142
ryan-demario <ryandemario1112@gmail.com>
2025-11-03 14:48:27 -0500

    Update BinaryHeap.cpp
    Added the task struct and variables it will have

--- src/BinaryHeap.cpp
+++ src/BinaryHeap.cpp
@@ -1,5 +1,20 @@
 //
 // 
 //
-
 #include "BinaryHeap.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Task {
+    int id;
+    string name;
+    int priority;
+    Task(int i, string n, int p) {
+        int id = i;
+        string name = n;
+        int priority = p;
+    }
+};
